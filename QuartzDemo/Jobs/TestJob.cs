325f6b34fc2feae194b0ce517d4299bda49ff07c
0Kelvins <554961401@qq.com>
2017-10-23 11:54:14 +0800

    upload

---
+++ QuartzDemo/Jobs/TestJob.cs
@@ -0,0 +1,20 @@
+﻿using log4net;
+using Quartz;
+using System;
+
+namespace QuartzDemo
+{
+    /// <summary>
+    /// 实现IJob接口
+    /// </summary>
+    public class TestJob : IJob
+    {
+        private readonly ILog _logger = LogManager.GetLogger(typeof(TestJob));
+
+        public void Execute(IJobExecutionContext context)
+        {
+            _logger.InfoFormat("TestJob测试");
+        }
+
+    }
+}
